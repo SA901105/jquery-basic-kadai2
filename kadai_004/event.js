5591e3be280b458d37a88136486b9cd0974070a1
SA901105 <157731270+SA901105@users.noreply.github.com>
2024-03-17 00:04:49 +0900

    課題oo4提出

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,13 +1,11 @@
-$(function() {
-  $(window).on({
-  // HTMLドキュメントが読み込まれたときのイベント
-  'load': () => {
-    console.log('loadイベントが発生しました');
-  },
+// HTMLドキュメントが読み込まれたときに「loadイベントが発生しました」とコンロールに表示
+$(window).on('load', function(){
+  console.log('loadイベントが発生しました');
+});
 
-  // 画面をスクロールしたときのイベント
-  'scroll': () => {
-    console.log('scrollイベントが発生しました');
-  },
+// 画面をスクロールしたときに「'scrollイベントが発生しました'」とコンソールに表示する
+$(function() {
+$(window).on('scroll', (e) =>{
+  console.log('scrollイベントが発生しました');
 });
 });
